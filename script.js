6f627be33980f9d184579cb095c0e382e3f3bb25
Leonardo G. Samartino <leonardog.samartino@gmail.com>
2024-04-28 20:18:44 -0300

    Alterações de design e estilização, começamos a codar a search bar

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+let champList = [];
+
+//Search bar
+const search = document.getElementById("search")
+search.addEventListener('keyup', (e) => {
+    const searchString = e.target.value
+    const filteredChampions = champList.filter((champion) => {
+        return champion.name.includes(searchString)
+    })
+    console.log(filteredChampions)
+})
+
+
 // Função para consumir os dados da API 
 const getChampionData = async function () {
     let champData = `https://ddragon.leagueoflegends.com/cdn/14.8.1/data/pt_BR/champion.json`
     const response = await fetch(champData)
-    const champList = await response.json()
+    champList = await response.json()
     return champList.data
 }
 
@@ -18,43 +31,32 @@ const CreatCards = async function (champListJSON) {
         let nameBox = document.createElement('h1')
         let splashArtBox = document.createElement('figure')
         let titleBox = document.createElement('div')
-        let loreBox = document.createElement('p')
-        let classBox = document.createElement('h3')
-        let diffBox = document.createElement('span')
+        let nameTitleBox = document.createElement('div')
 
         // elementos filho 
         let textName = document.createTextNode(champion.name)
         let splashArt = document.createElement('img')
         let title = document.createTextNode(champion.title)
-        let lore = document.createTextNode(champion.blurb)
-        let classeTag = document.createTextNode(champion.tags)
-        let difficulty = document.createTextNode(champion.info.difficulty)
 
         //adicionando atributos no HTML
         divCardBox.setAttribute('class', 'card_box')
         nameBox.setAttribute('class', 'name_box')
         splashArtBox.setAttribute('class', 'art_box')
         titleBox.setAttribute('class', 'title_box')
-        loreBox.setAttribute('class', 'lore_box')
-        splashArt.setAttribute('src', `https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/splash-art`)
-        classBox.setAttribute('class', 'class_box')
-        diffBox.setAttribute('class', 'diff_box')
+        splashArt.setAttribute('src', `https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/tile`)
+        nameTitleBox.setAttribute('class', 'name_titleBox')
 
 
         // alocando as divs dentro das respectivas divs pai
         divCard.appendChild(divCardBox)
         divCardBox.appendChild(splashArtBox)
         splashArtBox.appendChild(splashArt)
-        divCardBox.appendChild(diffBox)
-        // diffBox.appendChild(difficulty)
-        divCardBox.appendChild(classBox)
-        //classBox.appendChild(classeTag)
-        // divCardBox.appendChild(loreBox)
-        // loreBox.appendChild(lore)
-        divCardBox.appendChild(nameBox)
         nameBox.appendChild(textName)
         titleBox.appendChild(title)
-        divCardBox.appendChild(titleBox)
+        nameTitleBox.appendChild(nameBox)
+        nameTitleBox.appendChild(titleBox)
+        divCardBox.appendChild(nameTitleBox)
+
 
         // Adicionando evento de clique a cada card
         divCardBox.addEventListener('click', function () {
@@ -79,11 +81,13 @@ function displayModal(champion) {
     modalContent.innerHTML = `
         
         <img src="https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/splash-art" alt="${champion.name}" >
+        <div class="popupText">
         <h2>${champion.name}</h2>
         <h3>${champion.title}</h3>
         <p>${champion.blurb}</p>
         <h4>Tags: ${champion.tags.join(', ')}</h4>
-        <span>Dificuldade: ${champion.info.difficulty}</span>`;
+        <span>Dificuldade: ${champion.info.difficulty}</span>
+        </div>`;
 
     // Exibir o modal
     popup.style.display = "block";
@@ -98,5 +102,4 @@ window.onclick = function (event) {
         popup.style.display = "none";
     }
 }
-
 
