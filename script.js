a545a0b591c6b549898c7b8f8bbad6f6b21f4f59
Leonardo G. Samartino <leonardog.samartino@gmail.com>
2024-04-29 19:36:51 -0300

    Search bar funcionando, limpeza do codigo feita

--- script.js
+++ script.js
@@ -7,91 +7,91 @@ const getChampionData = async function () {
 }
 
 // Função para criar os Cards
-const CreatCards = async function (champListJSON) {
+const CreateCards = async function (champListJSON, searchTerm = '') {
     let divCard = document.getElementById('divCard')
+    
+    //propriedade para limpar a div container durante o filtro
+     divCard.innerHTML = ''
+    
+    let filteredChampions = champListJSON
+    if (searchTerm !== ''){
+        filteredChampions = filterChampions(champListJSON,searchTerm)
+    }
 
+    //forEach para criação automatizada dos cards
     champListJSON.forEach(function (champion) {
-        // div corpo do card
+        // Div corpo do card
         let divCardBox = document.createElement('div')
 
-        // elementos pai, para conter o conteúdo do card
+        // Containers
         let nameBox = document.createElement('h1')
         let splashArtBox = document.createElement('figure')
         let titleBox = document.createElement('div')
-        let loreBox = document.createElement('p')
-        let classBox = document.createElement('h3')
-        let diffBox = document.createElement('span')
+        let nameTitleBox = document.createElement('div')
 
-        // elementos filho 
+        // Elementos filho 
         let textName = document.createTextNode(champion.name)
         let splashArt = document.createElement('img')
         let title = document.createTextNode(champion.title)
-        let lore = document.createTextNode(champion.blurb)
-        let classeTag = document.createTextNode(champion.tags)
-        let difficulty = document.createTextNode(champion.info.difficulty)
 
-        //adicionando atributos no HTML
+        //Adicionando atributos no HTML
         divCardBox.setAttribute('class', 'card_box')
         nameBox.setAttribute('class', 'name_box')
         splashArtBox.setAttribute('class', 'art_box')
         titleBox.setAttribute('class', 'title_box')
-        loreBox.setAttribute('class', 'lore_box')
-        splashArt.setAttribute('src', `https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/splash-art`)
-        classBox.setAttribute('class', 'class_box')
-        diffBox.setAttribute('class', 'diff_box')
+        splashArt.setAttribute('src', `https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/tile`)
+        nameTitleBox.setAttribute('class', 'name_titleBox')
 
 
-        // alocando as divs dentro das respectivas divs pai
+        // Alocando as divs dentro dos respectivos containers
         divCard.appendChild(divCardBox)
         divCardBox.appendChild(splashArtBox)
+        divCardBox.appendChild(nameTitleBox)
         splashArtBox.appendChild(splashArt)
-        divCardBox.appendChild(diffBox)
-        // diffBox.appendChild(difficulty)
-        divCardBox.appendChild(classBox)
-        //classBox.appendChild(classeTag)
-        // divCardBox.appendChild(loreBox)
-        // loreBox.appendChild(lore)
-        divCardBox.appendChild(nameBox)
+        nameTitleBox.appendChild(nameBox)
+        nameTitleBox.appendChild(titleBox)
         nameBox.appendChild(textName)
         titleBox.appendChild(title)
-        divCardBox.appendChild(titleBox)
 
-        // Adicionando evento de clique a cada card
+
+        // Adicionando evento de clique a cada card para que exiba o pop-up com detalhes
         divCardBox.addEventListener('click', function () {
-            // Exibir o modal com os detalhes do campeão
             displayModal(champion)
         });
     })
 }
-// Função de callback para criar os cards quando a janela carregar
-window.addEventListener('load', async function () {
-    const champListJSON = await getChampionData()
-    await CreatCards(Object.values(champListJSON))
 
-})
-
-// Função para exibir o modal com os detalhes do campeão
-function displayModal(champion) {
+// Função para exibir o pop-up com os detalhes do campeão
+async function  displayModal(champion) {
     var popup = document.getElementById("popup");
     var modalContent = document.getElementById("popupContent");
 
-    // Preencher o conteúdo do modal com os detalhes do campeão
+    // Preencher o conteúdo do pop-up com os detalhes do campeão
     modalContent.innerHTML = `
         
         <img src="https://cdn.communitydragon.org/14.8.1/champion/${champion.id}/splash-art" alt="${champion.name}" >
+        <div class="popupText">
         <h2>${champion.name}</h2>
         <h3>${champion.title}</h3>
-        <p>${champion.blurb}</p>
+        <p id= "lore"></p>
         <h4>Tags: ${champion.tags.join(', ')}</h4>
-        <span>Dificuldade: ${champion.info.difficulty}</span>`;
+        <span>Dificuldade: ${champion.info.difficulty}</span>
+        </div>
+        `;
 
-    // Exibir o modal
+    // Exibir o pop-up
     popup.style.display = "block";
+
+    // Fetch em outra url para trazer a lore completa do campeão
+    const loreUrl = `https://ddragon.leagueoflegends.com/cdn/14.8.1/data/pt_BR/champion/${champion.id}.json`;
+    
+        const response = await fetch(loreUrl);
+        const data = await response.json();
+        const lore = data.data[champion.id].lore;
+        document.getElementById("lore").innerText = lore;
 }
 
-
-
-// Função para fechar o modal quando o usuário clicar fora dele
+// Função para fechar o pop-up quando o usuário clicar fora dele
 window.onclick = function (event) {
     var popup = document.getElementById("popup");
     if (event.target == popup) {
@@ -99,4 +99,18 @@ window.onclick = function (event) {
     }
 }
 
+// Função de callback para criar os cards quando a janela carregar
+window.addEventListener('load', async function () {
+    const champListJSON = await getChampionData()
+    await CreateCards(Object.values(champListJSON))
 
+    //evento de imput na barra de pesquisa
+    const searchBar = document.getElementById('search');
+    searchBar.addEventListener('input',function(){
+        const searchTerm = searchBar.value.toLowerCase()
+        const filteredChampions = Object.values(champListJSON).filter(champion => {
+          return champion.name.toLowerCase().includes(searchTerm)})
+
+        CreateCards(filteredChampions)
+    })
+})
